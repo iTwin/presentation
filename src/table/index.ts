48dd743c539252916007612c2bd2403fdaa797c4
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2018-08-08 09:02:02 +0300

    Move all ECPresentation code under `presentation` subdirectory

--- src/table/index.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
- *--------------------------------------------------------------------------------------------*/
-/** @module Content */
-
-export { default as PresentationTableDataProvider } from "./DataProvider";
-export { default as withUnifiedSelection } from "./WithUnifiedSelection";
--- src/tree/index.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-|  $Copyright: (c) 2018 Bentley Systems, Incorporated. All rights reserved. $
- *--------------------------------------------------------------------------------------------*/
-/** @module Hierarchies */
-
-export { default as PresentationTreeDataProvider } from "./DataProvider";
-export { default as IPresentationTreeDataProvider } from "./IPresentationTreeDataProvider";
-export { default as withUnifiedSelection, SelectionTarget } from "./WithUnifiedSelection";
-export { default as withFilteringSupport } from "./WithFilteringSupport";
