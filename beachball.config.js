692985fbeddcfab1092b29f79b23c2fb1895b204
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2022-12-05 16:05:38 +0200

    Add beachball

---
+++ beachball.config.js
@@ -0,0 +1,31 @@
+/** @type {import("beachball").BeachballConfig } */
+module.exports = {
+  publish: false,
+  gitTags: false,
+  disallowedChangeTypes: [
+    "major"
+  ],
+  scope: [
+    "packages/**"
+  ],
+  ignorePatterns: [
+    ".nycrc",
+    ".eslintrc.json",
+    ".mocharc.json",
+    "tsconfig.*",
+    ".*ignore",
+    ".github/**",
+    ".vscode/**",
+    "**/test/**",
+    "pnpm-lock.yaml",
+  ],
+  changehint: "Run 'pnpm change' to generate a change file",
+  changelog: {
+    customRenderers: {
+      renderEntry: (entry) => {
+        const commitLink = `https://github.com/iTwin/presentation/commit/${entry.commit}`;
+        return `- ${entry.comment} ([commit](${commitLink}))`
+      },
+    },
+  },
+};
