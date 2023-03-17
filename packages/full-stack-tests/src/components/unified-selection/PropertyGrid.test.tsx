1defaa378eb921e2c964365bbccf6ca811fd6c35
Grigas <35135765+grigasp@users.noreply.github.com>
2023-03-17 09:27:26 +0200

    Add full stack tests for unified selection components. Extract code snippets from them.

---
+++ packages/full-stack-tests/src/components/unified-selection/PropertyGrid.test.tsx
@@ -0,0 +1,130 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { expect } from "chai";
+import React from "react";
+import { UiComponents, VirtualizedPropertyGridWithDataProvider } from "@itwin/components-react";
+import { IModelApp, IModelConnection } from "@itwin/core-frontend";
+import { useDisposable } from "@itwin/core-react";
+import { InstanceKey, KeySet } from "@itwin/presentation-common";
+import { PresentationPropertyDataProvider, usePropertyDataProviderWithUnifiedSelection } from "@itwin/presentation-components";
+import { Presentation } from "@itwin/presentation-frontend";
+import { buildTestIModel } from "@itwin/presentation-testing";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import { insertPhysicalElement, insertPhysicalModel, insertSpatialCategory } from "../../IModelUtils";
+import { initialize, terminate } from "../../IntegrationTests";
+
+/* eslint-disable @typescript-eslint/naming-convention */
+
+describe("Learning snippets", async () => {
+
+  describe("Unified selection", () => {
+
+    describe("Property grid", () => {
+
+      before(async () => {
+        await initialize();
+        await UiComponents.initialize(IModelApp.localization);
+      });
+
+      after(async () => {
+        await terminate();
+      });
+
+      it("renders unified selection property grid", async function () {
+        // __PUBLISH_EXTRACT_START__ Presentation.Components.UnifiedSelection.PropertyGrid
+        function MyPropertyGrid(props: { imodel: IModelConnection }) {
+          // create a presentation rules driven data provider; the provider implements `IDisposable`, so we
+          // create it through `useDisposable` hook to make sure it's properly cleaned up
+          const dataProvider = useDisposable(React.useCallback(() => {
+            return new PresentationPropertyDataProvider({ imodel: props.imodel });
+          }, [props.imodel]));
+
+          // set up the data provider to be notified about changes in unified selection
+          const { isOverLimit, numSelectedElements } = usePropertyDataProviderWithUnifiedSelection({ dataProvider });
+
+          // width and height should generally we computed using ResizeObserver API or one of its derivatives
+          const [width] = React.useState(400);
+          const [height] = React.useState(600);
+
+          // data provider is going to be empty is no elements are selected
+          if (numSelectedElements === 0)
+            return <>Select an element to see its properties</>;
+
+          // there's little value in loading properties for many elements (see `PropertyDataProviderWithUnifiedSelectionProps.requestedContentInstancesLimit`)
+          if (isOverLimit)
+            return <>Please select less elements</>;
+
+          // render the property grid
+          return (
+            <VirtualizedPropertyGridWithDataProvider
+              dataProvider={dataProvider}
+              width={width}
+              height={height}
+            />
+          );
+        }
+        // __PUBLISH_EXTRACT_END__
+
+        // set up imodel for the test
+        const elementKeys: InstanceKey[] = [];
+        const imodel = await buildTestIModel(this, (builder) => {
+          const categoryKey = insertSpatialCategory(builder, "My Category");
+          const modelKey = insertPhysicalModel(builder, "My Model");
+          elementKeys.push(
+            insertPhysicalElement(builder, "My Element 1", modelKey.id, categoryKey.id),
+            insertPhysicalElement(builder, "My Element 2", modelKey.id, categoryKey.id),
+          );
+        });
+
+        // render the component
+        const { container, getByText } = render(
+          <MyPropertyGrid imodel={imodel} />
+        );
+        await waitFor(() => getByText("Select an element to see its properties"));
+
+        // test Unified Selection -> Property Grid content synchronization
+        act(() => Presentation.selection.replaceSelection("", imodel, new KeySet([elementKeys[0]])));
+        await ensurePropertyGridHasPropertyRecord(container, "User Label", "My Element 1");
+
+        act(() => Presentation.selection.replaceSelection("", imodel, new KeySet([elementKeys[1]])));
+        await ensurePropertyGridHasPropertyRecord(container, "User Label", "My Element 2");
+      });
+
+    });
+
+  });
+
+});
+
+async function ensurePropertyGridHasPropertyRecord(container: HTMLElement, propertyLabel: string, propertyValue: string) {
+  // find & expand the root category
+  const category = await waitFor(() => getRootPropertyCategory(container));
+  if (!category.querySelector(".iui-expanded"))
+    fireEvent.click(category.querySelector(".iui-expandable-block .iui-header")!);
+  await waitFor(() => expect(category.querySelector(".iui-expanded")).to.not.be.null);
+
+  // find the property label
+  await waitFor(() => {
+    const record = getPropertyRecordByLabel(container, propertyLabel);
+    expect(record.querySelector(`.components-property-record-value [title="${propertyValue}"]`)).to.not.be.null;
+  });
+}
+
+function getRootPropertyCategory(htmlContainer: HTMLElement) {
+  const categoryElement = htmlContainer.querySelector(`.virtualized-grid-node-category`);
+  if (!categoryElement)
+    throw new Error(`Failed to find root category`);
+  return categoryElement;
+}
+
+function getPropertyRecordByLabel(htmlContainer: HTMLElement, label: string) {
+  let curr = htmlContainer.querySelector(`.virtualized-grid-node .components-property-record-label [title*="${label}"]`);
+  while (curr && !curr.classList.contains("virtualized-grid-node"))
+    curr = curr.parentElement;
+  if (!curr || !curr.classList.contains("virtualized-grid-node"))
+    throw new Error(`Failed to find property record with property label "${label}"`);
+  return curr;
+}
---
+++ packages/full-stack-tests/src/components/unified-selection/Tree.test.tsx
@@ -0,0 +1,157 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { expect } from "chai";
+import React from "react";
+import { ControlledTree, SelectionMode, UiComponents, useTreeModel } from "@itwin/components-react";
+import { IModelApp, IModelConnection } from "@itwin/core-frontend";
+import { InstanceKey, KeySet, Ruleset, RuleTypes } from "@itwin/presentation-common";
+import { usePresentationTreeNodeLoader, useUnifiedSelectionTreeEventHandler } from "@itwin/presentation-components";
+import { Presentation } from "@itwin/presentation-frontend";
+import { buildTestIModel } from "@itwin/presentation-testing";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import { insertPhysicalElement, insertPhysicalModel, insertSpatialCategory } from "../../IModelUtils";
+import { initialize, terminate } from "../../IntegrationTests";
+
+/* eslint-disable @typescript-eslint/naming-convention */
+
+describe("Learning snippets", async () => {
+
+  describe("Unified selection", () => {
+
+    describe("Tree", () => {
+
+      before(async () => {
+        await initialize();
+        await UiComponents.initialize(IModelApp.localization);
+      });
+
+      after(async () => {
+        await terminate();
+      });
+
+      it("renders unified selection tree", async function () {
+        // __PUBLISH_EXTRACT_START__ Presentation.Components.UnifiedSelection.Tree
+        function MyTree(props: { imodel: IModelConnection }) {
+          // create a node loader for given iModel and ruleset
+          const { nodeLoader } = usePresentationTreeNodeLoader({ imodel: props.imodel, ruleset, pagingSize: 10 });
+
+          // get tree model to be rendered based on nodes loaded by the tree node loader
+          const treeModel = useTreeModel(nodeLoader.modelSource);
+
+          // create a tree events handler that synchronizes tree nodes' selection with unified selection
+          const treeEventsHandler = useUnifiedSelectionTreeEventHandler({ nodeLoader });
+
+          // width and height should generally we computed using ResizeObserver API or one of its derivatives
+          const [width] = React.useState(400);
+          const [height] = React.useState(600);
+
+          return (
+            <ControlledTree
+              nodeLoader={nodeLoader}
+              model={treeModel}
+              eventsHandler={treeEventsHandler}
+              width={width}
+              height={height}
+              selectionMode={SelectionMode.Extended}
+            />
+          );
+        }
+        // __PUBLISH_EXTRACT_END__
+
+        // set up imodel for the test
+        let modelKey: InstanceKey;
+        let elementKey: InstanceKey;
+        const imodel = await buildTestIModel(this, (builder) => {
+          const categoryKey = insertSpatialCategory(builder, "My Category");
+          modelKey = insertPhysicalModel(builder, "My Model");
+          elementKey = insertPhysicalElement(builder, "My Element", modelKey.id, categoryKey.id);
+        });
+
+        // render the component
+        const { container, getByRole } = render(
+          <MyTree imodel={imodel} />
+        );
+        await waitFor(() => getByRole("tree"));
+
+        // find & expand the model node
+        const modelNode = await waitFor(() => getNodeByLabel(container, "My Model"));
+        fireEvent.click(modelNode.querySelector(".core-tree-expansionToggle")!);
+        // find the element node
+        const elementNode = await waitFor(() => getNodeByLabel(container, "My Element"));
+
+        // test Unified Selection -> Tree selection synchronization
+        act(() => Presentation.selection.replaceSelection("", imodel, new KeySet([modelKey!])));
+        await waitFor(() => {
+          expect(isNodeSelectedInTree(modelNode!)).to.be.true;
+          expect(isNodeSelectedInTree(elementNode)).to.be.false;
+        });
+        act(() => Presentation.selection.replaceSelection("", imodel, new KeySet([elementKey!])));
+        await waitFor(() => {
+          expect(isNodeSelectedInTree(modelNode!)).to.be.false;
+          expect(isNodeSelectedInTree(elementNode)).to.be.true;
+        });
+
+        // test Tree selection -> Unified Selection synchronization
+        fireEvent.click(modelNode);
+        await waitFor(() => {
+          expect(getInstanceKeysInUnifiedSelection(imodel)).to.deep.eq([modelKey]);
+        });
+        fireEvent.click(elementNode);
+        await waitFor(() => {
+          expect(getInstanceKeysInUnifiedSelection(imodel)).to.deep.eq([elementKey]);
+        });
+      });
+
+    });
+
+  });
+
+});
+
+function getInstanceKeysInUnifiedSelection(imodel: IModelConnection) {
+  const map = Presentation.selection.getSelection(imodel).instanceKeys;
+  const arr = new Array<InstanceKey>();
+  map.forEach((ids, className) => ids.forEach((id) => arr.push({ className, id })));
+  return arr;
+}
+
+function getNodeByLabel(htmlContainer: HTMLElement, label: string) {
+  let curr = htmlContainer.querySelector(`[title*="${label}"]`);
+  while (curr && !curr.classList.contains("core-tree-node"))
+    curr = curr.parentElement;
+  if (!curr || !curr.classList.contains("core-tree-node"))
+    throw new Error(`Failed to find node with label "${label}"`);
+  return curr;
+}
+
+function isNodeSelectedInTree(htmlElement: Element) {
+  return htmlElement.classList.contains("is-selected");
+}
+
+const ruleset: Ruleset = {
+  id: "elements-grouped-by-models",
+  rules: [{
+    ruleType: RuleTypes.RootNodes,
+    specifications: [{
+      specType: "InstanceNodesOfSpecificClasses",
+      classes: { schemaName: "BisCore", classNames: ["PhysicalModel"], arePolymorphic: true },
+      groupByClass: false,
+      groupByLabel: false,
+    }],
+  }, {
+    ruleType: RuleTypes.ChildNodes,
+    condition: "ParentNode.IsOfClass(\"Model\", \"BisCore\")",
+    specifications: [{
+      specType: "RelatedInstanceNodes",
+      relationshipPaths: [{
+        relationship: { schemaName: "BisCore", className: "ModelContainsElements" },
+        direction: "Forward",
+      }],
+      groupByClass: false,
+      groupByLabel: false,
+    }],
+  }],
+};
