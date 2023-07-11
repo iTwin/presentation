63ca3633934b2df9135096513830c6c0ae0824f4
simjar <124659623+simasjar@users.noreply.github.com>
2023-07-11 16:45:51 +0300

    add comments explaining validation

--- packages/components/src/presentation-components/properties/NumericPropertyInput.tsx
+++ packages/components/src/presentation-components/properties/NumericPropertyInput.tsx
@@ -65,18 +65,23 @@ export interface NumericInputProps {
 export const NumericInput = ({ value, onChange }: NumericInputProps) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const val = e.currentTarget.value;
+    // Check if it is a correct number and it is not infinity.
     if (!isNaN(Number(val)) && isFinite(Number(val))) {
       onChange(val);
     }
+    // Number{"+"), Number("-") and Number(".") returns NaN, but if input is only `.`, `-` or `+`, we should fire `onChange` function.
     else if (val.length === 1 && "+-.".includes(val)) {
       onChange(val);
     }
+    // Number("+.") and Number("-.") returns NaN, but if input is only `+.` or `-.`, we want to fire `onChange` function.
     else if (val === "+." || val === "-.") {
       onChange(val);
     }
+    // Let user write scientific numbers. Number("1e") returns NaN, but we want to fire `onChange` function when input before `e` is a correct number.
     else if (val.endsWith("e") && !isNaN(Number(val.slice(0, val.length - 1))) && val.length !== 1) {
       onChange(val);
     }
+    // Let user write scientific numbers. Number("1e-") returns NaN, but we want to fire `onChange` function when input before `e-` is a correct number.
     else if (val.endsWith("e-")) {
       onChange(val);
     }
