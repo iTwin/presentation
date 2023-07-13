5a290497f52ff3dd214011389ebfbfb49146e16b
Anmol Shrestha <anmol.shrestha@bentley.com>
2023-07-13 13:38:05 -0400

    add copyright info in config scripts

--- packages/components/eslint.config.js
+++ packages/components/eslint.config.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 const iTwinPlugin = require("@itwin/eslint-plugin");
 const eslintBaseConfig = require("../../eslint.base.config");
 const jsxRuntimeConfig = require("eslint-plugin-react");
