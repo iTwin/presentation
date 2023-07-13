329be0730f2c177c3e66b12b67e0bea06cd1c16c
Anmol Shrestha <anmol.shrestha@bentley.com>
2023-07-13 13:43:42 -0400

    remove unnecessary customLanguage options

--- apps/test-app/backend/eslint.config.js
+++ apps/test-app/backend/eslint.config.js
@@ -5,19 +5,6 @@
 const iTwinPlugin = require("@itwin/eslint-plugin");
 const eslintBaseConfig = require("../../../eslint.base.config");
 
-const customLanguageOptions = {
-  sourceType: "module",
-  parser: require("@typescript-eslint/parser"),
-  parserOptions: {
-    project: ["./tsconfig.json"],
-    ecmaVersion: "latest",
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-    },
-  },
-};
-
 module.exports = [
   ...eslintBaseConfig,
   {
--- apps/test-app/frontend/eslint.config.js
+++ apps/test-app/frontend/eslint.config.js
@@ -6,20 +6,6 @@ const iTwinPlugin = require("@itwin/eslint-plugin");
 const eslintBaseConfig = require("../../../eslint.base.config");
 const jsxRuntimeConfig = require("eslint-plugin-react");
 
-
-const customLanguageOptions = {
-  sourceType: "module",
-  parser: require("@typescript-eslint/parser"),
-  parserOptions: {
-    project: ["./tsconfig.json"],
-    ecmaVersion: "latest",
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-    },
-  },
-};
-
 module.exports = [
   ...eslintBaseConfig,
   {
