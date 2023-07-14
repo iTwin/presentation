143871928a1ff99e5fbb838f4e4b296d7a24e587
grigas.petraitis <35135765+grigasp@users.noreply.github.com>
2023-07-14 09:33:41 +0300

    Fix prettier issues

--- packages/components/src/test/tree/controlled/UseControlledTreeFiltering.test.ts
+++ packages/components/src/test/tree/controlled/UseControlledTreeFiltering.test.ts
@@ -13,7 +13,10 @@ import { NodePathElement } from "@itwin/presentation-common";
 import { waitFor } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks";
 import { IPresentationTreeDataProvider } from "../../../presentation-components/tree/IPresentationTreeDataProvider";
-import { ControlledPresentationTreeFilteringProps, useControlledPresentationTreeFiltering } from "../../../presentation-components/tree/controlled/UseControlledTreeFiltering";
+import {
+  ControlledPresentationTreeFilteringProps,
+  useControlledPresentationTreeFiltering,
+} from "../../../presentation-components/tree/controlled/UseControlledTreeFiltering";
 import { FilteredPresentationTreeDataProvider } from "../../../presentation-components/tree/FilteredDataProvider";
 import { ResolvablePromise } from "../../_helpers/Promises";
 import { createTestPropertyRecord, createTestTreeNodeItem } from "../../_helpers/UiComponents";
