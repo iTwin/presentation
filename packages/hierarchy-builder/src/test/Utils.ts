0827b55d8a54d3fa456d4374705686c462f624bd
Grigas <35135765+grigasp@users.noreply.github.com>
2023-09-14 17:55:40 +0300

    Hierarchy builder: Introduce instance label select clause factory (#263)
    * Introduce instance label select clause factory
    
    * empty changeset

--- packages/hierarchy-builder/src/test/Utils.ts
+++ packages/hierarchy-builder/src/test/Utils.ts
@@ -4,8 +4,12 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Observable } from "rxjs";
+import sinon from "sinon";
+import { Id64, Id64String } from "@itwin/core-bentley";
+import { ECClass, SchemaContext } from "@itwin/ecschema-metadata";
 import { InstanceKey } from "../hierarchy-builder/EC";
 import { HierarchyNode } from "../hierarchy-builder/HierarchyNode";
+import * as common from "../hierarchy-builder/internal/Common";
 
 export async function getObservableResult<T>(obs: Observable<T>): Promise<Array<T>> {
   const arr = new Array<T>();
@@ -42,4 +46,50 @@ export function createTestInstanceKey(src?: Partial<InstanceKey>): InstanceKey {
     id: "0x1",
     ...src,
   };
+}
+
+export interface TStubClassFuncProps {
+  schemaName: string;
+  className: string;
+  classLabel?: string;
+  is?: (fullClassName: string) => Promise<boolean>;
+}
+export interface TStubClassFuncReturnType {
+  id: Id64String;
+  name: string;
+  label: string;
+}
+export type TStubClassFunc = (props: TStubClassFuncProps) => TStubClassFuncReturnType;
+export function createGetClassStub(schemas: SchemaContext) {
+  const stub = sinon.stub(common, "getClass");
+  const stubClass: TStubClassFunc = (props) => {
+    const fullName = `${props.schemaName}:${props.className}`;
+    const fullNameMatcher = sinon.match((fullClassName: string) => {
+      const { schemaName, className } = parseFullClassName(fullClassName);
+      return schemaName === props.schemaName && className === props.className;
+    });
+    stub.withArgs(schemas, fullNameMatcher).resolves({
+      fullName,
+      name: props.className,
+      label: props.classLabel,
+      is: sinon.fake(async (targetClass: ECClass) => {
+        if (!props.is) {
+          return false;
+        }
+        const { schemaName, className } = parseFullClassName(targetClass.fullName);
+        return props.is(`${schemaName}.${className}`);
+      }),
+    } as unknown as ECClass);
+    return {
+      id: Id64.invalid,
+      name: fullName,
+      label: props.classLabel ?? props.className,
+    };
+  };
+  return { getClass: stub, stubClass };
+}
+
+function parseFullClassName(fullClassName: string) {
+  const [schemaName, className] = fullClassName.split(/[\.:]/);
+  return { schemaName, className };
 }
