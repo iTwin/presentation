f2f1e5a3a764525d73bc21334a7e85abacf7a0cc
JonasDov <100586436+JonasDov@users.noreply.github.com>
2023-10-05 17:39:47 +0300

    createLabelGrouping doesn't do any sorting

--- packages/hierarchy-builder/src/hierarchy-builder/internal/operators/LabelGrouping.ts
+++ packages/hierarchy-builder/src/hierarchy-builder/internal/operators/LabelGrouping.ts
@@ -3,12 +3,10 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import naturalCompare from "natural-compare-lite";
 import { from, mergeMap, Observable, tap, toArray } from "rxjs";
 import { HierarchyNode } from "../../HierarchyNode";
 import { getLogger } from "../../Logging";
 import { createOperatorLoggingNamespace } from "../Common";
-import { sortNodesByLabelOperator } from "./Sorting";
 
 const OPERATOR_NAME = "Grouping.ByLabel";
 /** @internal */
@@ -27,9 +25,7 @@ export function createLabelGroupingOperator() {
       mergeMap((groupings) => {
         const grouped = createGroupingNodes(groupings);
         const obs = from(grouped);
-        // source observable is expected to stream sorted nodes and we're keeping them in order - only
-        // need to re-sort if we created grouping nodes
-        return grouped.hasLabelGroupingNodes ? obs.pipe(sortNodesByLabelOperator) : obs;
+        return obs;
       }),
       log((n) => `out: ${n.label}`),
     );
@@ -39,20 +35,21 @@ export function createLabelGroupingOperator() {
 interface LabelGroupingInformation {
   ungrouped: Array<HierarchyNode>;
   grouped: Map<string, { label: string; groupedNodes: Array<HierarchyNode> }>;
+  order: Array<string>;
 }
 
 async function createLabelGroupingInformation(nodes: HierarchyNode[]): Promise<LabelGroupingInformation> {
-  const groupings: LabelGroupingInformation = { ungrouped: [], grouped: new Map() };
+  const groupings: LabelGroupingInformation = { ungrouped: [], grouped: new Map(), order: [] };
   for (const node of nodes) {
     if (HierarchyNode.isClassGroupingNode(node) && Array.isArray(node.children)) {
       const labelGroupings = await createLabelGroupingInformation(node.children);
       const labelGroupingNodes = createGroupingNodes(labelGroupings);
-      const sortedNodes = labelGroupingNodes.sort((lhs, rhs) => naturalCompare(lhs.label.toLocaleLowerCase(), rhs.label.toLocaleLowerCase()));
       const newClassGroupingNode: HierarchyNode = {
         ...node,
-        children: sortedNodes,
+        children: labelGroupingNodes,
       };
       groupings.ungrouped.push(newClassGroupingNode);
+      groupings.order.push("ungrouped");
     } else if (node.params?.groupByLabel) {
       const nodeLabel = node.label;
       let groupingInfo = groupings.grouped.get(nodeLabel);
@@ -62,15 +59,17 @@ async function createLabelGroupingInformation(nodes: HierarchyNode[]): Promise<L
           groupedNodes: [],
         };
         groupings.grouped.set(nodeLabel, groupingInfo);
+        groupings.order.push("grouped");
       }
       groupingInfo.groupedNodes.push(node);
     } else {
+      groupings.order.push("ungrouped");
       groupings.ungrouped.push(node);
     }
   }
   // if all nodes have the same label, then they should not be grouped
   if (groupings.grouped.size === 1 && groupings.ungrouped.length === 0) {
-    return { ungrouped: nodes, grouped: new Map() };
+    return { ungrouped: nodes, grouped: new Map(), order: [] };
   }
 
   return groupings;
@@ -78,13 +77,20 @@ async function createLabelGroupingInformation(nodes: HierarchyNode[]): Promise<L
 
 function createGroupingNodes(groupings: LabelGroupingInformation): HierarchyNode[] & { hasLabelGroupingNodes?: boolean } {
   const outNodes = new Array<HierarchyNode>();
+  if (groupings.order.length === 0) {
+    outNodes.push(...groupings.ungrouped);
+    (outNodes as any).hasLabelGroupingNodes = false;
+    return outNodes;
+  }
+
+  const tempOutNodes = new Array<HierarchyNode>();
   let isGroupingNodeCreated = false;
   groupings.grouped.forEach((entry) => {
     // if group contains 1 node, then the grouping should not be created
     if (entry.groupedNodes.length === 1) {
-      outNodes.push(...entry.groupedNodes);
+      tempOutNodes.push(...entry.groupedNodes);
     } else {
-      outNodes.push({
+      tempOutNodes.push({
         label: entry.label,
         key: {
           type: "label-grouping",
@@ -95,7 +101,20 @@ function createGroupingNodes(groupings: LabelGroupingInformation): HierarchyNode
       isGroupingNodeCreated = true;
     }
   });
-  outNodes.push(...groupings.ungrouped);
+  tempOutNodes.push(...groupings.ungrouped);
+
+  let ungroupedNodeIndex = 0;
+  let groupedNodeIndex = 0;
+  groupings.order.forEach((nodeType) => {
+    if (nodeType === "grouped") {
+      outNodes.push(tempOutNodes[groupedNodeIndex]);
+      ++groupedNodeIndex;
+    } else {
+      outNodes.push(tempOutNodes[groupings.grouped.size + ungroupedNodeIndex]);
+      ++ungroupedNodeIndex;
+    }
+  });
+
   (outNodes as any).hasLabelGroupingNodes = isGroupingNodeCreated;
   return outNodes;
 }
