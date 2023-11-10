aab01dcf0cfce617ca465a2ce4799277ae60ba11
github-action-benchmark <github@users.noreply.github.com>
2023-11-10 15:14:48 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1f1e8cdd0b3d4f823df8ca18be42ed535d498f35

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1699629288444,
+  "repoUrl": "https://github.com/iTwin/presentation",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Grigas",
+            "username": "grigasp",
+            "email": "35135765+grigasp@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "1f1e8cdd0b3d4f823df8ca18be42ed535d498f35",
+          "message": "Allow manually dispatching perf benchmark workflow (#335)",
+          "timestamp": "2023-11-10T15:13:01Z",
+          "url": "https://github.com/iTwin/presentation/commit/1f1e8cdd0b3d4f823df8ca18be42ed535d498f35"
+        },
+        "date": 1699629287456,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Initial Models Tree Load: Baytown.bim",
+            "value": 361,
+            "unit": "ms",
+            "extra": "min: 361\nmax: 361\ncount: 1\nmean: 361\np50: 361.5\nmedian: 361.5\np75: 361.5\np90: 361.5\np95: 361.5\np99: 361.5\np999: 361.5"
+          },
+          {
+            "name": "Full Models Tree Load: Baytown.bim",
+            "value": 1845,
+            "unit": "ms",
+            "extra": "min: 1845\nmax: 1845\ncount: 1\nmean: 1845\np50: 1826.6\nmedian: 1826.6\np75: 1826.6\np90: 1826.6\np95: 1826.6\np99: 1826.6\np999: 1826.6"
+          }
+        ]
+      }
+    ]
+  }
+}
