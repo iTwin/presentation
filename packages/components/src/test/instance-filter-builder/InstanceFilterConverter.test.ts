bc7de4d1efab973c605ac0d2bd5d76e466ce04fd
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-02-12 10:36:08 +0200

    lint

--- packages/components/src/test/instance-filter-builder/InstanceFilterConverter.test.ts
+++ packages/components/src/test/instance-filter-builder/InstanceFilterConverter.test.ts
@@ -9,12 +9,10 @@ import { PropertyValue, PropertyValueFormat, StandardTypeNames } from "@itwin/ap
 import { PropertyFilterRuleGroupOperator, PropertyFilterRuleOperator } from "@itwin/components-react";
 import { BeEvent } from "@itwin/core-bentley";
 import { IModelConnection } from "@itwin/core-frontend";
-import { ClassInfo, PropertyValueFormat as TypeValueFormat, RelationshipPath, Value } from "@itwin/presentation-common";
+import { ClassInfo, RelationshipPath, PropertyValueFormat as TypeValueFormat, Value } from "@itwin/presentation-common";
 import { ECClassInfo, getIModelMetadataProvider } from "../../presentation-components/instance-filter-builder/ECMetadataProvider";
 import {
-  PresentationInstanceFilter,
-  PresentationInstanceFilterCondition,
-  PresentationInstanceFilterConditionGroup,
+  PresentationInstanceFilter, PresentationInstanceFilterCondition, PresentationInstanceFilterConditionGroup,
 } from "../../presentation-components/instance-filter-builder/PresentationFilterBuilder";
 import { createTestECClassInfo, createTestPropertyInfo } from "../_helpers/Common";
 import { createTestNestedContentField, createTestPropertiesContentField } from "../_helpers/Content";
