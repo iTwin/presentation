bbc3e7eda1ccd8fbd414058ad89374814e4924bf
JonasDov <100586436+JonasDov@users.noreply.github.com>
2024-02-15 01:11:57 +0200

    Use Dictionary instead of Map in BaseClassChecker

--- packages/hierarchy-builder/src/hierarchy-builder/Utils.ts
+++ packages/hierarchy-builder/src/hierarchy-builder/Utils.ts
@@ -3,6 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import { compareStrings, Dictionary } from "@itwin/core-bentley";
 import { ECClass, IMetadataProvider } from "./ECMetadata";
 import { getClass } from "./internal/Common";
 
@@ -12,19 +13,32 @@ import { getClass } from "./internal/Common";
  */
 export type OmitOverUnion<T, K extends PropertyKey> = T extends T ? Omit<T, K> : never;
 
+interface BaseClassCheckerDictionaryKey {
+  ecClassNameToCheck: string;
+  baseECClass: ECClass;
+}
+
 /** @beta */
 export class BaseClassChecker {
-  private _map = new Map<string, boolean>();
+  private _map = new Dictionary<BaseClassCheckerDictionaryKey, boolean>((lhs, rhs) => this.compareKeys(lhs, rhs));
+
+  private compareKeys(lhs: BaseClassCheckerDictionaryKey, rhs: BaseClassCheckerDictionaryKey) {
+    const classNameCompareResult = compareStrings(lhs.ecClassNameToCheck, rhs.ecClassNameToCheck);
+    if (classNameCompareResult !== 0) {
+      return classNameCompareResult;
+    }
+    return compareStrings(lhs.baseECClass.fullName, rhs.baseECClass.fullName);
+  }
 
   public async isECClassOfBaseECClass(metadata: IMetadataProvider, ecClassNameToCheck: string, baseECClass: ECClass): Promise<boolean> {
-    let isCurrentNodeClassOfBase = this._map.get(ecClassNameToCheck);
+    let isCurrentNodeClassOfBase = this._map.get({ ecClassNameToCheck, baseECClass });
     if (isCurrentNodeClassOfBase === undefined) {
       const currentNodeECClass = await getClass(metadata, ecClassNameToCheck);
       if (await currentNodeECClass.is(baseECClass)) {
-        this._map.set(ecClassNameToCheck, true);
+        this._map.set({ ecClassNameToCheck, baseECClass }, true);
         isCurrentNodeClassOfBase = true;
       } else {
-        this._map.set(ecClassNameToCheck, false);
+        this._map.set({ ecClassNameToCheck, baseECClass }, false);
         isCurrentNodeClassOfBase = false;
       }
     }
