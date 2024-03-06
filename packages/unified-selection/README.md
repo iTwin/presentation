a08465f17e94585eac67c6194bc2dccc60d2e084
Dominykas Jasiunas <33428304+jasdom@users.noreply.github.com>
2024-03-06 11:26:47 +0200

    Added unified-selection package

---
+++ packages/unified-selection/README.md
@@ -0,0 +1,7 @@
+# @itwin/unified-selection
+
+Copyright © Bentley Systems, Incorporated. All rights reserved. See LICENSE.md for license terms and full copyright notice.
+
+## Description
+
+The **@itwin/unified-selection** package provides API for managing [unified selection](https://www.itwinjs.org/presentation/unified-selection/).
---
+++ packages/unified-selection/src/unified-selection.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+/**
+ * @module UnifiedSelection
+ *
+ * @docs-group-description UnifiedSelection
+ * API for working with unified selection within [React](https://reactjs.org/) components.
+ */
+export * from "./unified-selection";
