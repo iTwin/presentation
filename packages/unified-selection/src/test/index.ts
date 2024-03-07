40d481306f9746bc5d53739205433546f241a955
Dominykas Jasiūnas <33428304+jasdom@users.noreply.github.com>
2024-03-07 18:17:47 +0200

    Added unified-selection package (#460)

---
+++ packages/unified-selection/src/test/index.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import sinonChai from "sinon-chai";
+
+// setup chai
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
