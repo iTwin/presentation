cf846b4ab91abe43eb4c5a34d6dc4a59f5f3fbfb
Dmitrij Kuzmiciov <yato333@users.noreply.github.com>
2024-03-08 16:49:18 +0200

    Improve comment

--- apps/performance-tests/src/util/TestUtilities.ts
+++ apps/performance-tests/src/util/TestUtilities.ts
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-/** Runs a test and measures the time it takes and the amount of time main thread is blocked.  */
+/** Runs a test and passes information about it to the TestReporter. */
 export function itMeasures(desc: string, callback: (() => void) | (() => Promise<void>)) {
   it(desc, async function () {
     this.test!.ctx!.testReporterOnTestStart();
