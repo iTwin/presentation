05f442ce22e0c4c1b262963ecccbc7022bb14ce6
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-03-13 17:05:39 +0200

    Add missing headers

--- packages/hierarchies-react/src/presentation-hierarchies-react.ts
+++ packages/hierarchies-react/src/presentation-hierarchies-react.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 export * from "./presentation-hierarchies-react/Tree";
 export * from "./presentation-hierarchies-react/Types";
 export * from "./presentation-hierarchies-react/UseTree";
