c265582591b915bc9b32d188cf71537c1197ceb1
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-03-13 18:12:25 +0200

    Initial `@itwin/presentation-hierarchies-react` setup (#474)
    * Move `useTree` API to new package
    
    * Add icon support
    
    * Fix unified selection
    
    * Revive hierarchy level filtering
    
    * Add missing headers
    
    * Remove commented code
    
    * Setup API extraction
    
    * Remove test script
    
    * Add missing header

---
+++ packages/hierarchies-react/src/presentation-hierarchies-react/Tree.tsx
@@ -0,0 +1,162 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import "./Tree.css";
+import cx from "classnames";
+import { ComponentPropsWithoutRef, ReactElement, useCallback } from "react";
+import { SvgFilter, SvgFilterHollow, SvgRemove } from "@itwin/itwinui-icons-react";
+import { Button, IconButton, ProgressRadial, Tree, TreeNode } from "@itwin/itwinui-react";
+import { isPresentationHierarchyNode, PresentationHierarchyNode, PresentationTreeNode } from "./Types";
+import { HierarchyLevelFilteringOptions, useTree } from "./UseTree";
+
+interface TreeRendererProps extends Omit<ReturnType<typeof useTree>, "rootNodes" | "isLoading"> {
+  rootNodes: PresentationTreeNode[];
+  onFilterClick: (filteringInfo: HierarchyLevelFilteringOptions) => void;
+  getIcon?: (node: PresentationHierarchyNode) => ReactElement | undefined;
+}
+
+/** @beta */
+export function TreeRenderer({
+  rootNodes,
+  expandNode,
+  selectNode,
+  isNodeSelected,
+  setHierarchyLevelLimit,
+  getHierarchyLevelFilteringOptions,
+  removeHierarchyLevelFilter,
+  onFilterClick,
+  getIcon,
+}: TreeRendererProps) {
+  const nodeRenderer = useCallback<TreeProps<PresentationTreeNode>["nodeRenderer"]>(
+    ({ node, ...restProps }) => {
+      if (isPresentationHierarchyNode(node)) {
+        return (
+          <TreeNode
+            {...restProps}
+            className={cx("stateless-tree-node", { filtered: node.isFiltered })}
+            label={node.label}
+            onExpanded={(_, isExpanded) => {
+              expandNode(node.id, isExpanded);
+            }}
+            onSelected={(_, isSelected) => {
+              selectNode(node.id, isSelected);
+            }}
+            icon={getIcon ? getIcon(node) : undefined}
+          >
+            {node.isFiltered ? (
+              <IconButton
+                className="filtering-action-button"
+                styleType="borderless"
+                size="small"
+                onClick={(e) => {
+                  removeHierarchyLevelFilter(node.id);
+                  e.stopPropagation();
+                }}
+              >
+                <SvgRemove />
+              </IconButton>
+            ) : null}
+            {node.isFilterable ? (
+              <IconButton
+                className="filtering-action-button"
+                styleType="borderless"
+                size="small"
+                onClick={(e) => {
+                  const options = getHierarchyLevelFilteringOptions(node.id);
+                  if (options) {
+                    onFilterClick(options);
+                  }
+                  e.stopPropagation();
+                }}
+              >
+                {node.isFiltered ? <SvgFilter /> : <SvgFilterHollow />}
+              </IconButton>
+            ) : null}
+          </TreeNode>
+        );
+      }
+
+      if (node.type === "ChildrenPlaceholder") {
+        return <PlaceholderNode {...restProps} label={node.message} />;
+      }
+
+      if (node.type === "ResultSetTooLarge") {
+        return <ResultSetTooLargeNode {...restProps} label={node.message} onRemoveLimit={() => setHierarchyLevelLimit(node.parentNodeId, "unbounded")} />;
+      }
+      return <TreeNode {...restProps} label={node.message} isDisabled={true} onExpanded={() => {}} />;
+    },
+    [expandNode, selectNode, setHierarchyLevelLimit, getHierarchyLevelFilteringOptions, removeHierarchyLevelFilter, onFilterClick, getIcon],
+  );
+
+  const getNode = useCallback<TreeProps<PresentationTreeNode>["getNode"]>(
+    (node) => {
+      if (!isPresentationHierarchyNode(node)) {
+        return {
+          nodeId: node.id,
+          node,
+          hasSubNodes: false,
+          isExpanded: false,
+          isSelected: false,
+          isDisabled: true,
+        };
+      }
+      return {
+        nodeId: node.id,
+        node,
+        hasSubNodes: node.children === true || node.children.length > 0,
+        subNodes:
+          // returns placeholder node to show as child while children is loading.
+          node.children === true
+            ? [
+                {
+                  id: `Loading-${node.id}`,
+                  parentNodeId: node.id,
+                  type: "ChildrenPlaceholder",
+                  message: "Loading...",
+                },
+              ]
+            : node.children,
+        isExpanded: node.isExpanded,
+        isSelected: isNodeSelected(node.id),
+      };
+    },
+    [isNodeSelected],
+  );
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        overflow: "auto",
+      }}
+    >
+      <Tree<PresentationTreeNode> data={rootNodes} nodeRenderer={nodeRenderer} getNode={getNode} enableVirtualization={true} />
+    </div>
+  );
+}
+
+function PlaceholderNode(props: Omit<TreeNodeProps, "onExpanded">) {
+  return <TreeNode {...props} icon={<ProgressRadial size="x-small" indeterminate />} onExpanded={() => {}}></TreeNode>;
+}
+
+function ResultSetTooLargeNode({ onRemoveLimit, ...props }: Omit<TreeNodeProps, "onExpanded"> & { onRemoveLimit: () => void }) {
+  return (
+    <TreeNode {...props} onExpanded={() => {}}>
+      <Button
+        styleType="borderless"
+        size="small"
+        onClick={(e) => {
+          onRemoveLimit();
+          e.stopPropagation();
+        }}
+      >
+        Remove Limit
+      </Button>
+    </TreeNode>
+  );
+}
+
+type TreeProps<T> = ComponentPropsWithoutRef<typeof Tree<T>>;
+type TreeNodeProps = ComponentPropsWithoutRef<typeof TreeNode>;
---
+++ packages/hierarchies-react/src/presentation-hierarchies-react/UnifiedSelectionContext.tsx
@@ -0,0 +1,66 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
+import { Key } from "@itwin/presentation-common";
+
+interface UnifiedSelectionContext {
+  has: (keys: Key[]) => boolean;
+  add: (keys: Key[]) => void;
+  remove: (keys: Key[]) => void;
+}
+
+const unifiedSelectionContext = createContext<UnifiedSelectionContext>({
+  has: () => false,
+  add: () => {},
+  remove: () => {},
+});
+
+/** @beta */
+export interface EventLike<TArgs> {
+  addListener: (listener: (args: TArgs) => void) => () => void;
+}
+
+/** @beta */
+export interface UnifiedSelectionContainer {
+  add: (keys: Key[]) => void;
+  remove: (keys: Key[]) => void;
+  has: (keys: Key[]) => boolean;
+}
+
+/** @beta */
+export interface UnifiedSelectionStore {
+  onChange: EventLike<UnifiedSelectionContainer>;
+  container: UnifiedSelectionContainer;
+}
+
+interface Props {
+  store: UnifiedSelectionStore;
+}
+
+/** @beta */
+export function UnifiedSelectionContextProvider({ store, children }: PropsWithChildren<Props>) {
+  const [value, setValue] = useState<UnifiedSelectionContext>({
+    has: (keys) => store.container.has(keys),
+    add: (keys) => store.container.add(keys),
+    remove: (keys) => store.container.remove(keys),
+  });
+
+  useEffect(() => {
+    return store.onChange.addListener((container) => {
+      setValue({
+        has: (keys) => container.has(keys),
+        add: (keys) => container.add(keys),
+        remove: (keys) => container.remove(keys),
+      });
+    });
+  }, [store]);
+
+  return <unifiedSelectionContext.Provider value={value}>{children}</unifiedSelectionContext.Provider>;
+}
+
+/** @internal */
+export function useUnifiedSelectionContext() {
+  return useContext(unifiedSelectionContext);
+}
