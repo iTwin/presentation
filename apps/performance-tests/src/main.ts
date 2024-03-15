44f333011bab967d0e4591f57bb9ade99b4f1d28
Dmitrij Kuzmiciov <yato333@users.noreply.github.com>
2024-03-15 16:42:10 +0200

    Add logging

--- apps/performance-tests/src/main.ts
+++ apps/performance-tests/src/main.ts
@@ -3,13 +3,17 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import "chai";
 import { IModelHost } from "@itwin/core-backend";
 import { Logger, LogLevel } from "@itwin/core-bentley";
+import { createLogger } from "@itwin/presentation-core-interop";
+import { setLogger } from "@itwin/presentation-hierarchy-builder";
 import { Datasets } from "./Datasets";
 
 before(async () => {
   Logger.initializeToConsole();
   Logger.setLevelDefault(LogLevel.Error);
+  setLogger(createLogger());
   await IModelHost.startup({
     profileName: "presentation-performance-tests",
   });
