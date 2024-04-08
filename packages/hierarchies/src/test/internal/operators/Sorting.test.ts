e48c67ba080c36958acc11380ebeff2591b423a1
Grigas <35135765+grigasp@users.noreply.github.com>
2024-04-08 14:03:48 +0300

    hierarchies: Change `HierarchyProvider.getNodes` from `Promise<HierarchyNode[]>` to `AsyncIterableIterator<HierarchyNode>` (#516)

--- packages/hierarchies/src/test/internal/operators/Sorting.test.ts
+++ packages/hierarchies/src/test/internal/operators/Sorting.test.ts
@@ -6,12 +6,12 @@
 import { expect } from "chai";
 import { from } from "rxjs";
 import { sortNodesByLabelOperator } from "../../../hierarchies/internal/operators/Sorting";
-import { createTestProcessedCustomNode, getObservableResult } from "../../Utils";
+import { collect, createTestProcessedCustomNode } from "../../Utils";
 
 describe("Sorting", () => {
   it("sorts nodes", async () => {
     const nodes = [createTestProcessedCustomNode({ label: "b" }), createTestProcessedCustomNode({ label: "c" }), createTestProcessedCustomNode({ label: "a" })];
-    const result = await getObservableResult(from(nodes).pipe(sortNodesByLabelOperator));
+    const result = await collect(from(nodes).pipe(sortNodesByLabelOperator));
     expect(result).to.deep.eq([nodes[2], nodes[0], nodes[1]]);
   });
 });
