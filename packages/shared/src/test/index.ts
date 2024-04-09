3fb9546d1ec0e7bb20e306a58c9c2525de99f198
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2024-04-09 09:22:59 +0300

    set up a `presentation-shared` package

---
+++ packages/shared/src/test/index.ts
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import chaiSubset from "chai-subset";
+import sinonChai from "sinon-chai";
+
+// setup chai
+chai.use(chaiAsPromised);
+chai.use(chaiSubset);
+chai.use(sinonChai);
