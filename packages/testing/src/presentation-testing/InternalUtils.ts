4aab2a3844dab9ea817873fe9268b4df64582ad3
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2024-06-03 11:50:47 +0300

    `testing`: When creating a test iModel, limit its file name length to fit into file system limits.

--- packages/testing/src/presentation-testing/InternalUtils.ts
+++ packages/testing/src/presentation-testing/InternalUtils.ts
@@ -27,19 +27,36 @@ export function setTestOutputDir(directoryPath: string | undefined) {
  * - Resolving the output file name under the known test output directory
  * - Making directories as necessary
  * - Removing a previous copy of the output file
- * @param fileName Name of output file
+ * @param fileName Name of output file. The actual file name may get modified to fit within the file system limits.
  * @internal
  */
 export function setupOutputFileLocation(fileName: string): LocalFileName {
   const outputDirectoryPath = getTestOutputDir();
   !IModelJsFs.existsSync(outputDirectoryPath) && IModelJsFs.mkdirSync(outputDirectoryPath);
 
-  const outputFile = path.join(outputDirectoryPath, `${fileName}.bim`);
-  IModelJsFs.existsSync(outputFile) && IModelJsFs.unlinkSync(outputFile);
-  return outputFile;
+  const outputFilePath = limitFilePathLength(path.join(outputDirectoryPath, `${fileName}.bim`));
+  IModelJsFs.existsSync(outputFilePath) && IModelJsFs.unlinkSync(outputFilePath);
+  return outputFilePath;
 }
 
 /** @internal */
 export function createFileNameFromString(str: string) {
   return sanitize(str.replace(/[ ]+/g, "-").replaceAll("`", "").replaceAll("'", "")).toLocaleLowerCase();
+}
+
+/** @internal */
+export function limitFilePathLength(filePath: string) {
+  const { dir, name, ext } = path.parse(filePath);
+
+  const allowedFileNameLength = 260 - 12 - 1 - (dir.length + 1) - ext.length;
+  if (allowedFileNameLength <= 0) {
+    throw new Error(`File path "${filePath}" is too long.`);
+  }
+  if (name.length < allowedFileNameLength) {
+    return filePath;
+  }
+
+  const pieceLength = (allowedFileNameLength - 3) / 2;
+  const shortenedName = `${name.slice(0, pieceLength)}...${name.slice(name.length - pieceLength)}`;
+  return path.join(dir, `${shortenedName}${ext}`);
 }
