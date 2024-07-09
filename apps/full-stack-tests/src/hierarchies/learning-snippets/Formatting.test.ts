62bce862920829d9c1afd6032993ad08f3d69db8
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2024-07-09 16:58:30 +0300

    Unify learning snippets

--- apps/full-stack-tests/src/hierarchies/learning-snippets/Formatting.test.ts
+++ apps/full-stack-tests/src/hierarchies/learning-snippets/Formatting.test.ts
@@ -5,7 +5,7 @@
 /* eslint-disable no-duplicate-imports */
 
 import { expect } from "chai";
-import { collect, insertPhysicalElement, insertPhysicalModelWithPartition, insertSpatialCategory } from "presentation-test-utilities";
+import { insertPhysicalElement, insertPhysicalModelWithPartition, insertSpatialCategory } from "presentation-test-utilities";
 // __PUBLISH_EXTRACT_START__ Presentation.Hierarchies.Formatting.BasicFormatterExample.Imports
 import { createDefaultValueFormatter, IPrimitiveValueFormatter } from "@itwin/presentation-shared";
 // __PUBLISH_EXTRACT_END__
@@ -14,11 +14,12 @@ import { createValueFormatter } from "@itwin/presentation-core-interop";
 // __PUBLISH_EXTRACT_END__
 // __PUBLISH_EXTRACT_START__ Presentation.Hierarchies.Formatting.NodeLabelFormattingExamples.Imports
 import { createHierarchyProvider, createNodesQueryClauseFactory } from "@itwin/presentation-hierarchies";
-import { createDefaultInstanceLabelSelectClauseFactory, ECSql } from "@itwin/presentation-shared";
+import { ECSql } from "@itwin/presentation-shared";
 // __PUBLISH_EXTRACT_END__
 import { buildIModel, importSchema } from "../../IModelUtils";
 import { initialize, terminate } from "../../IntegrationTests";
 import { createIModelAccess, createSchemaContext } from "../Utils";
+import { collectHierarchy } from "./Utils";
 
 describe("Hierarchies", () => {
   describe("Learning snippets", () => {
@@ -36,18 +37,13 @@ describe("Hierarchies", () => {
         const defaultFormatter = createDefaultValueFormatter();
         const myFormatter: IPrimitiveValueFormatter = async (value) => {
           if (value.type === "Boolean") {
-            return value.value ? "yes" : "no";
+            return value.value ? "yes!" : "no!";
           }
           return defaultFormatter(value);
         };
-        // returns "yes"
-        const formattedYes = await myFormatter({ type: "Boolean", value: true });
-        // returns "no"
-        const formattedNo = await myFormatter({ type: "Boolean", value: false });
+        expect(await myFormatter({ type: "Boolean", value: true })).to.eq("yes!");
+        expect(await myFormatter({ type: "Boolean", value: false })).to.eq("no!");
         // __PUBLISH_EXTRACT_END__
-
-        expect(formattedYes).to.eq("yes");
-        expect(formattedNo).to.eq("no");
       });
 
       it("formats values with units", async function () {
@@ -71,10 +67,10 @@ describe("Hierarchies", () => {
         const metricFormatter = createValueFormatter({ schemaContext, unitSystem: "metric" });
         const imperialFormatter = createValueFormatter({ schemaContext, unitSystem: "imperial" });
 
-        // define the raw value to be formatted
+        // Define the raw value to be formatted
         const value = 1.234;
 
-        // define the KindOfQuantity to use for formatting:
+        // Define the KindOfQuantity to use for formatting:
         // <KindOfQuantity
         //   typeName="FlowRate"
         //   displayLabel="Flow Rate"
@@ -84,19 +80,15 @@ describe("Hierarchies", () => {
         // />
         const koqName = `${mySchemaName}.FlowRate`;
 
-        // not passing `koqName` formats the value without units using the default formatter - returns `1.23`
-        const formattedWithoutUnits = await metricFormatter({ type: "Double", value });
+        // Not passing `koqName` formats the value without units using the default formatter:
+        expect(await metricFormatter({ type: "Double", value })).to.eq("1.23");
 
-        // metric formatter formats the value in liters per minute - returns `74040.0 L/min`
-        const formattedInMetric = await metricFormatter({ type: "Double", value, koqName });
+        // Metric formatter formats the value in liters per minute:
+        expect(await metricFormatter({ type: "Double", value, koqName })).to.eq("74040.0 L/min");
 
-        // imperial formatter formats the value in gallons per minute - returns `19559.2988 gal/min`
-        const formattedInImperial = await imperialFormatter({ type: "Double", value, koqName });
+        // Imperial formatter formats the value in gallons per minute:
+        expect(await imperialFormatter({ type: "Double", value, koqName })).to.eq("19559.2988 gal/min");
         // __PUBLISH_EXTRACT_END__
-
-        expect(formattedWithoutUnits).to.eq("1.23");
-        expect(formattedInMetric).to.eq("74040.0 L/min");
-        expect(formattedInImperial).to.eq("19559.2988 gal/min");
       });
 
       it("formats custom node's concatenated value label", async function () {
@@ -107,41 +99,44 @@ describe("Hierarchies", () => {
         const hierarchyProvider = createHierarchyProvider({
           imodelAccess,
           hierarchyDefinition: {
-            defineHierarchyLevel: async () => [
-              // the hierarchy definition returns a single node with a ConcatenatedValue-based label
-              {
-                node: {
-                  key: "custom node",
-                  label: [
-                    "Example | ",
-                    {
-                      type: "Integer",
-                      value: 123,
+            defineHierarchyLevel: async ({ parentNode }) => {
+              if (!parentNode) {
+                return [
+                  // The hierarchy definition returns a single node with a ConcatenatedValue-based label
+                  {
+                    node: {
+                      key: "custom node",
+                      label: [
+                        "Example | ",
+                        {
+                          type: "Integer",
+                          value: 123,
+                        },
+                        {
+                          type: "String",
+                          value: " | ",
+                        },
+                        {
+                          type: "Point2d",
+                          value: { x: 1, y: 2 },
+                        },
+                      ],
                     },
-                    {
-                      type: "String",
-                      value: " | ",
-                    },
-                    {
-                      type: "Point2d",
-                      value: { x: 1, y: 2 },
-                    },
-                  ],
-                },
-              },
-            ],
+                  },
+                ];
+              }
+              return [];
+            },
           },
         });
 
-        // returns a single node with label "Example | 123 | (1.00, 2.00)"
-        const nodes = hierarchyProvider.getNodes({ parentNode: undefined });
+        // Returns the node with formatted and concatenated label:
+        expect(await collectHierarchy(hierarchyProvider)).to.deep.eq([{ label: "Example | 123 | (1.00, 2.00)" }]);
         // __PUBLISH_EXTRACT_END__
-
-        expect((await collect(nodes)).map((node) => node.label)).to.deep.eq([`Example | 123 | (${Number(1).toFixed(2)}, ${Number(2).toFixed(2)})`]);
       });
 
       it("formats instance node's concatenated value label", async function () {
-        const { imodel, ...keys } = await buildIModel(this, async (builder) => {
+        const { imodel } = await buildIModel(this, async (builder) => {
           const category = insertSpatialCategory({ builder, codeValue: "Example category" });
           return { category };
         });
@@ -151,43 +146,46 @@ describe("Hierarchies", () => {
         const hierarchyProvider = createHierarchyProvider({
           imodelAccess,
           hierarchyDefinition: {
-            defineHierarchyLevel: async () => [
-              // the hierarchy definition returns BisCore.SpatialCategory nodes
-              {
-                fullClassName: "BisCore.SpatialCategory",
-                query: {
-                  ecsql: `
-                    SELECT ${await createNodesQueryClauseFactory({ imodelAccess }).createSelectClause({
-                      ecClassId: { selector: "this.ECClassId" },
-                      ecInstanceId: { selector: "this.ECInstanceId" },
-                      // generally, one of the `IInstanceLabelSelectClauseFactory` implementations, delivered with `@itwin/presentation-shared` package, should be used,
-                      // but for demonstration purposes, a custom implementation is used here
-                      nodeLabel: {
-                        selector: ECSql.createConcatenatedValueJsonSelector([
-                          // create a selector for `CodeValue` property value
-                          { propertyClassName: "BisCore.SpatialCategory", propertyClassAlias: "this", propertyName: "CodeValue" },
-                          // include a static string value
-                          { type: "String", value: " [" },
-                          // create a selector for `ECInstanceId` property value in hex format
-                          { selector: `printf('0x%x', ${ECSql.createRawPropertyValueSelector("this", "ECInstanceId")})` },
-                          // include a static string value
-                          { type: "String", value: "]" },
-                        ]),
-                      },
-                    })}
-                    FROM BisCore.SpatialCategory this
-                  `,
-                },
-              },
-            ],
+            defineHierarchyLevel: async ({ parentNode }) => {
+              if (!parentNode) {
+                return [
+                  // The hierarchy definition returns `BisCore.SpatialCategory` nodes
+                  {
+                    fullClassName: "BisCore.SpatialCategory",
+                    query: {
+                      ecsql: `
+                        SELECT ${await createNodesQueryClauseFactory({ imodelAccess }).createSelectClause({
+                          ecClassId: { selector: "this.ECClassId" },
+                          ecInstanceId: { selector: "this.ECInstanceId" },
+                          // Generally, one of the `IInstanceLabelSelectClauseFactory` implementations, delivered with `@itwin/presentation-shared` package, should be used,
+                          // but for demonstration purposes, a custom implementation is used here
+                          nodeLabel: {
+                            selector: ECSql.createConcatenatedValueJsonSelector([
+                              // Create a selector for `CodeValue` property value
+                              { propertyClassName: "BisCore.SpatialCategory", propertyClassAlias: "this", propertyName: "CodeValue" },
+                              // Include a static string value
+                              { type: "String", value: " [" },
+                              // Create a selector for `ECInstanceId` property value in hex format
+                              { selector: `printf('0x%x', this.ECInstanceId)` },
+                              // Include a static string value
+                              { type: "String", value: "]" },
+                            ]),
+                          },
+                        })}
+                        FROM BisCore.SpatialCategory this
+                      `,
+                    },
+                  },
+                ];
+              }
+              return [];
+            },
           },
         });
 
-        // all returned nodes have their labels set in format "{CodeValue} [{ECInstanceId}]"
-        const nodes = hierarchyProvider.getNodes({ parentNode: undefined });
+        // All returned nodes have their labels set in format "{CodeValue} [{ECInstanceId}]":
+        expect(await collectHierarchy(hierarchyProvider)).to.deep.eq([{ label: "Example category [0x11]" }]);
         // __PUBLISH_EXTRACT_END__
-
-        expect((await collect(nodes)).map((node) => node.label)).to.deep.eq([`Example category [${keys.category.id}]`]);
       });
 
       it("formats property grouping node's label", async function () {
@@ -210,6 +208,7 @@ describe("Hierarchies", () => {
             modelId: model.id,
             categoryId: category.id,
             classFullName: schema.items.MyPhysicalObject.fullName,
+            userLabel: "Example element 1",
             doubleProperty: 123.45,
           });
           insertPhysicalElement({
@@ -217,6 +216,7 @@ describe("Hierarchies", () => {
             modelId: model.id,
             categoryId: category.id,
             classFullName: schema.items.MyPhysicalObject.fullName,
+            userLabel: "Example element 2",
             doubleProperty: 123.454,
           });
           return { myPhysicalObjectClassName: schema.items.MyPhysicalObject.fullName };
@@ -227,38 +227,46 @@ describe("Hierarchies", () => {
         const hierarchyProvider = createHierarchyProvider({
           imodelAccess,
           hierarchyDefinition: {
-            defineHierarchyLevel: async () => [
-              // the hierarchy definition returns nodes for `myPhysicalObjectClassName` element type, grouped by `DoubleProperty` property value
-              {
-                fullClassName: myPhysicalObjectClassName,
-                query: {
-                  ecsql: `
-                    SELECT ${await createNodesQueryClauseFactory({ imodelAccess }).createSelectClause({
-                      ecClassId: { selector: "this.ECClassId" },
-                      ecInstanceId: { selector: "this.ECInstanceId" },
-                      nodeLabel: { selector: await createDefaultInstanceLabelSelectClauseFactory().createSelectClause({ classAlias: "this" }) },
-                      grouping: {
-                        byProperties: {
-                          propertiesClassName: myPhysicalObjectClassName,
-                          propertyGroups: [{ propertyClassAlias: "this", propertyName: "DoubleProperty" }],
-                        },
-                      },
-                    })}
-                    FROM ${myPhysicalObjectClassName} this
-                  `,
-                },
-              },
-            ],
+            defineHierarchyLevel: async ({ parentNode }) => {
+              if (!parentNode) {
+                return [
+                  // The hierarchy definition returns nodes for `myPhysicalObjectClassName` element type, grouped by `DoubleProperty` property value
+                  {
+                    fullClassName: myPhysicalObjectClassName,
+                    query: {
+                      ecsql: `
+                        SELECT ${await createNodesQueryClauseFactory({ imodelAccess }).createSelectClause({
+                          ecClassId: { selector: "this.ECClassId" },
+                          ecInstanceId: { selector: "this.ECInstanceId" },
+                          nodeLabel: { selector: "this.UserLabel" },
+                          grouping: {
+                            byProperties: {
+                              propertiesClassName: myPhysicalObjectClassName,
+                              propertyGroups: [{ propertyClassAlias: "this", propertyName: "DoubleProperty" }],
+                            },
+                          },
+                        })}
+                        FROM ${myPhysicalObjectClassName} this
+                      `,
+                    },
+                  },
+                ];
+              }
+              return [];
+            },
           },
         });
 
         // The iModel has two elements of `myPhysicalObjectClassName` type, whose `DoubleProperty` values
         // are `123.450` and `123.454`. After passing through formatter, they both become equal to `123.45`,
-        // so we get one property grouping node for the two nodes.
-        const nodes = hierarchyProvider.getNodes({ parentNode: undefined });
+        // so we get one property grouping node for the two nodes:
+        expect(await collectHierarchy(hierarchyProvider)).to.deep.eq([
+          {
+            label: "123.45",
+            children: [{ label: "Example element 1" }, { label: "Example element 2" }],
+          },
+        ]);
         // __PUBLISH_EXTRACT_END__
-
-        expect((await collect(nodes)).map((node) => node.label)).to.deep.eq(["123.45"]);
       });
     });
   });
