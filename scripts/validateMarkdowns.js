62fb499f947afbae16aa2f50a34cd67628428823
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2024-09-30 14:16:01 +0300

    wip

--- scripts/validateMarkdowns.js
+++ scripts/validateMarkdowns.js
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 "use strict";
 
-const { spawn } = require("child_process");
+const { spawn, spawnSync } = require("child_process");
 const fg = require("fast-glob");
 
 const [_node, _script, ...targets] = process.argv;
@@ -18,29 +18,41 @@ let combinedOutput = "";
 
 const filePaths = targets.flatMap((pattern) => fg.sync(pattern, { dot: true }));
 
-Promise.all(
-  filePaths.map(
-    (filePath) =>
-      new Promise((resolve) => {
-        const p = spawn("npx", ["markdown-link-check", filePath], { shell: true, encoding: "utf8" });
+// Promise.all(
+//   filePaths.map(
+//     (filePath) =>
+//       new Promise((resolve) => {
+//         const p = spawn("npx", ["markdown-link-check", filePath], { shell: true, encoding: "utf8" });
+//
+//         let myStdout = "";
+//         p.stdout.on("data", (data) => (myStdout += data));
+//
+//         p.on("close", (code) => {
+//           combinedOutput += myStdout;
+//           hasError |= !!code;
+//           resolve();
+//         });
+//       }),
+//   ),
+// ).then(() => {
+//   process.stdout.write(combinedOutput);
+//
+//   console.log();
+//   if (hasError) {
+//     console.error(`Markdowns validation failed.`);
+//     process.exit(1);
+//   }
+//   console.log(`Markdowns validation succeeded.`);
+// });
 
-        let myStdout = "";
-        p.stdout.on("data", (data) => (myStdout += data));
-
-        p.on("close", (code) => {
-          combinedOutput += myStdout;
-          hasError |= !!code;
-          resolve();
-        });
-      }),
-  ),
-).then(() => {
-  process.stdout.write(combinedOutput);
-
-  console.log();
-  if (hasError) {
-    console.error(`Markdowns validation failed.`);
-    process.exit(1);
-  }
-  console.log(`Markdowns validation succeeded.`);
+filePaths.forEach((filePath) => {
+  const res = spawnSync("npx", ["markdown-link-check", filePath], { shell: true, stdio: "inherit" });
+  hasError |= !!res.status;
 });
+
+console.log();
+if (hasError) {
+  console.error(`Markdowns validation failed.`);
+  process.exit(1);
+}
+console.log(`Markdowns validation succeeded.`);
