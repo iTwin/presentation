1b51140ee194cd2a5f9087bc046760ee7543c59e
Grigas <35135765+grigasp@users.noreply.github.com>
2024-10-07 14:24:50 +0300

    Parallelize tests (#729)

--- packages/components/src/test/setup.ts
+++ packages/components/src/test/setup.ts
@@ -2,49 +2,53 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+// WARNING: The order of imports in this file is important!
 
+// setup chai
 import * as chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import chaiJestSnapshot from "chai-jest-snapshot";
 import chaiSubset from "chai-subset";
-import globalJsdom from "global-jsdom";
-import * as jsdom from "jsdom";
-import path from "path";
-import ResizeObserver from "resize-observer-polyfill";
 import sinonChai from "sinon-chai";
-
-// setup chai
 chai.use(chaiAsPromised);
 chai.use(chaiJestSnapshot);
 chai.use(sinonChai);
 chai.use(chaiSubset);
 
 // get rid of various xhr errors in the console
+import globalJsdom from "global-jsdom";
+import * as jsdom from "jsdom";
 globalJsdom(undefined, {
   virtualConsole: new jsdom.VirtualConsole().sendTo(console, { omitJSDOMErrors: true }),
 });
+
+// polyfill ResizeObserver
+import ResizeObserver from "resize-observer-polyfill";
 global.ResizeObserver = ResizeObserver;
 
-before(function () {
-  chaiJestSnapshot.resetSnapshotRegistry();
-  getGlobalThis().IS_REACT_ACT_ENVIRONMENT = true;
-});
-
-after(function () {
-  delete require.cache[__filename];
-  delete getGlobalThis().IS_REACT_ACT_ENVIRONMENT;
-});
-
-beforeEach(function () {
-  const currentTest = (this as unknown as Mocha.Context).currentTest!;
-
-  // set up snapshot name
-  const sourceFilePath = currentTest.file!.replace(`lib${path.sep}cjs${path.sep}test`, `src${path.sep}test`).replace(/\.(jsx?|tsx?)$/, "");
-  const snapPath = `${sourceFilePath}.snap`;
-  chaiJestSnapshot.setFilename(snapPath);
-  chaiJestSnapshot.setTestName(currentTest.fullTitle());
-});
-
+// supply mocha hooks
+import path from "path";
+import { cleanup } from "@testing-library/react";
+export const mochaHooks = {
+  beforeAll() {
+    chaiJestSnapshot.resetSnapshotRegistry();
+    getGlobalThis().IS_REACT_ACT_ENVIRONMENT = true;
+  },
+  beforeEach() {
+    // set up snapshot name
+    const currentTest = (this as unknown as Mocha.Context).currentTest!;
+    const sourceFilePath = currentTest.file!.replace(`lib${path.sep}cjs${path.sep}test`, `src${path.sep}test`).replace(/\.(jsx?|tsx?)$/, "");
+    const snapPath = `${sourceFilePath}.snap`;
+    chaiJestSnapshot.setFilename(snapPath);
+    chaiJestSnapshot.setTestName(currentTest.fullTitle());
+  },
+  afterEach() {
+    cleanup();
+  },
+  afterAll() {
+    delete getGlobalThis().IS_REACT_ACT_ENVIRONMENT;
+  },
+};
 function getGlobalThis(): typeof globalThis & { IS_REACT_ACT_ENVIRONMENT?: boolean } {
   /* istanbul ignore else */
   if (typeof globalThis !== "undefined") {
