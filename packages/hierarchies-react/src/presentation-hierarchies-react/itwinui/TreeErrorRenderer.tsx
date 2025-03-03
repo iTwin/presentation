72afc36500689e84b7555325e6d754ca2511b3bf
Martynas <43886789+MartynasStrazdas@users.noreply.github.com>
2025-03-03 16:58:48 +0200

    [Hierarchy-react]: kiwi tree sub label (#894)
    * initial
    
    * fix error node size
    
    * change

--- packages/hierarchies-react/src/presentation-hierarchies-react/itwinui/TreeErrorRenderer.tsx
+++ packages/hierarchies-react/src/presentation-hierarchies-react/itwinui/TreeErrorRenderer.tsx
@@ -134,7 +134,7 @@ function ErrorNodeLabel({ message, onRetry }: { message: string; onRetry?: () =>
   const { localizedStrings } = useLocalizationContext();
   return (
     <div style={{ display: "flex" }}>
-      <Text>{message}</Text>
+      <Text variant={"body-sm"}>{message}</Text>
       {onRetry ? <Anchor onClick={onRetry}>{localizedStrings?.retry}</Anchor> : null}
     </div>
   );
@@ -151,7 +151,7 @@ function createLocalizedMessage(message: string, limit: number, onClick?: () => 
       title: messageWithLimit,
       element: (
         <div style={{ display: "flex" }}>
-          <Text>{messageWithLimit}</Text>
+          <Text variant={"body-sm"}>{messageWithLimit}</Text>
         </div>
       ),
     };
@@ -164,7 +164,7 @@ function createLocalizedMessage(message: string, limit: number, onClick?: () => 
     title: messageWithLimit.replace(fullText, innerText),
     element: (
       <div style={{ display: "flex" }}>
-        {textBefore ? <Text>{textBefore}</Text> : null}
+        {textBefore ? <Text variant={"body-sm"}>{textBefore}</Text> : null}
         <Anchor
           // underline props does not exist in v5
           onClick={(e) => {
@@ -174,7 +174,7 @@ function createLocalizedMessage(message: string, limit: number, onClick?: () => 
         >
           {innerText}
         </Anchor>
-        {textAfter ? <Text>{textAfter}</Text> : null}
+        {textAfter ? <Text variant={"body-sm"}>{textAfter}</Text> : null}
       </div>
     ),
   };
