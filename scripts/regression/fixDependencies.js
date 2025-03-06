275bb2d8d8feb3a308a509b023338047df0902d7
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-03-06 13:09:35 +0200

    Add regression pipeline (#885)
    * Add regression pipeline that build with itwinjs-core ^4.10.0
    
    * Add build against 4.0.0
    
    * Override devDependencies instead of all
    
    * Update lock file
    
    * Log all failures
    
    * Build with 4.4.0
    
    * Run tests
    
    * Fix full-stack-tests-build
    
    * Remove 4.4.0 build
    
    * Handle older versions in full stack tests
    
    * Update script
    
    * Path build errors
    
    * Update favorite properties tests
    
    * Run regression test using locally built tarballs
    
    * No frozen lockfile
    
    * Update paths
    
    * Correct folder names
    
    * Add some logging
    
    * Flatten tarballs directory
    
    * Remove old regression pipeline
    
    * Apply patches based on version
    
    * Apply suggestions from code review
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * Address PR comments
    
    * Update diff
    
    * Build with different AppUI versions
    
    * Prettier
    
    * Fix lock-fie
    
    * Fix import
    
    * Prettier...
    
    * Replace `assert` -> `with`
    
    * Add lint ignore
    
    * Parse dependencies from workspace file
    
    ---------
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>

---
+++ scripts/regression/fixDependencies.js
@@ -0,0 +1,53 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+// this script makes it easier to use workspace packages with locally built tarballs by moving
+// direct workspace package dependencies to peerDependencies
+
+"use strict";
+
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+const packagesToUpdate = [
+  {
+    name: "@itwin/presentation-testing",
+    dependencies: [
+      {
+        name: "@itwin/presentation-components",
+        peerDependencyVersion: "^5.0.0",
+      },
+    ],
+  },
+];
+
+function forEachWorkspacePackage(callback) {
+  const workspaceProjects = JSON.parse(execSync("pnpm -r list --depth -1 --json"));
+  workspaceProjects.forEach((project) => {
+    callback(project);
+  });
+}
+
+forEachWorkspacePackage((project) => {
+  const expectedChanges = packagesToUpdate.find((packageToUpdate) => packageToUpdate.name === project.name);
+  if (!expectedChanges) {
+    return;
+  }
+
+  const packageJsonPath = path.join(project.path, "package.json");
+  const pkgJsonData = JSON.parse(fs.readFileSync(packageJsonPath, { encoding: "utf8" }));
+
+  expectedChanges.dependencies.forEach((dependency) => {
+    if (pkgJsonData.dependencies[dependency.name]) {
+      console.log(`Moving ${dependency.name} from dependencies to peerDependencies in ${pkgJsonData.name}`);
+      pkgJsonData.devDependencies[dependency.name] = pkgJsonData.dependencies[dependency.name];
+      pkgJsonData.peerDependencies[dependency.name] = dependency.peerDependencyVersion;
+      delete pkgJsonData.dependencies[dependency.name];
+    }
+  });
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(pkgJsonData, null, 2) + "\n", { encoding: "utf8" });
+});
