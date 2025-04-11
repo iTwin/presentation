13de7b5983cc32848409972b9bb8e2ecc91c51bd
Jonas <100586436+JonasDov@users.noreply.github.com>
2025-04-11 12:37:02 +0300

    Test6

--- scripts/filterBenchmarkComment.js
+++ scripts/filterBenchmarkComment.js
@@ -11,7 +11,7 @@ const path = require("path");
 const argv = yargs(process.argv).argv;
 const inputPath = path.resolve(argv.commentFilePath);
 
-const lines = fs.readFileSync(inputPath);
+const lines = JSON.parse(fs.readFileSync(inputPath));
 console.log(lines);
 // const result = lines.map((line) => {
 //   // Keep all lines that don't contain information about tests
