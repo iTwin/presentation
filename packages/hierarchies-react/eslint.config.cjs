5db3ca2e97d46fe1fa72b0c59a64afe641e3dc56
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-04-24 17:39:55 +0300

    Fix hierarchies-react

--- packages/hierarchies-react/eslint.config.cjs
+++ packages/hierarchies-react/eslint.config.cjs
@@ -15,5 +15,11 @@ module.exports = [
     files: ["**/*.{ts,tsx}"],
     ...reactPlugin.configs.flat["jsx-runtime"],
   },
+  {
+    files: ["src/test/**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
   ...eslintBaseConfig,
 ];
