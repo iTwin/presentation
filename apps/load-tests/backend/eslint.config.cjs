f80d2dda4b338d4ba957ed3a5635225ec749d1c2
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-04-24 18:04:12 +0300

    Fix load-tests

--- apps/load-tests/backend/eslint.config.cjs
+++ apps/load-tests/backend/eslint.config.cjs
@@ -10,5 +10,11 @@ module.exports = [
     files: ["**/*.ts"],
     ...iTwinPlugin.configs.iTwinjsRecommendedConfig,
   },
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
   ...eslintBaseConfig,
 ];
