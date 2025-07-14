8e2b959e902a0e2b129b403d32719041f952512e
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-07-14 15:36:49 +0300

    Removed dependencies that are not used in source files

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,4 +1,4 @@
-function readPackage(pkg, context) {
+function readPackage(pkg) {
   if (pkg.name === "@artilleryio/sketches-js") {
     // `@artilleryio/sketches-js` specifies `protobufjs` as a devDependency, although it should be a dependency...
     pkg.dependencies = {
@@ -7,48 +7,13 @@ function readPackage(pkg, context) {
     };
   }
 
-  // @itwin/components-react, @itwin/core-react, @itwin/imodel-components-react and @itwin/appui-react has peerDependencies of itwinjs-core set to v4.
-  // Need to change those peer dependencies to v5
-
-  const itwinjsCorePeerDependencyOverride = "^4.0.0 || ^5.0.0";
-  if (pkg.name === "@itwin/components-react") {
+  // `typedoc` used by `@itwin/build-tools` does not have 5.7.0 typescript as a peerDependency
+  if (pkg.name === "typedoc") {
     pkg.peerDependencies = {
       ...pkg.peerDependencies,
-      "@itwin/appui-abstract": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-bentley": itwinjsCorePeerDependencyOverride,
+      typescript: `${pkg.peerDependencies["typescript"]} || ^5.7.0`,
     };
   }
-  if (pkg.name === "@itwin/core-react") {
-    pkg.peerDependencies = {
-      ...pkg.peerDependencies,
-      "@itwin/appui-abstract": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-bentley": itwinjsCorePeerDependencyOverride,
-    };
-  }
-  if (pkg.name === "@itwin/imodel-components-react") {
-    pkg.peerDependencies = {
-      ...pkg.peerDependencies,
-      "@itwin/appui-abstract": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-bentley": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-common": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-frontend": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-geometry": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-quantity": itwinjsCorePeerDependencyOverride,
-    };
-  }
-
-  if (pkg.name === "@itwin/appui-react") {
-    pkg.peerDependencies = {
-      ...pkg.peerDependencies,
-      "@itwin/appui-abstract": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-bentley": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-common": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-frontend": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-geometry": itwinjsCorePeerDependencyOverride,
-      "@itwin/core-quantity": itwinjsCorePeerDependencyOverride,
-    };
-  }
-
   return pkg;
 }
 
