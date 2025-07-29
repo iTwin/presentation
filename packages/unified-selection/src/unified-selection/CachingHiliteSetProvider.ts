a603b2542e6a09b4e01cb34e18f2d7e32d7840a1
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2025-07-29 10:21:16 +0300

    Fix `enableUnifiedSelectionSyncWithIModel` incorrectly synchronizing selection on "add" and "remove", when custom hilite set provider is used

--- packages/unified-selection/src/unified-selection/CachingHiliteSetProvider.ts
+++ packages/unified-selection/src/unified-selection/CachingHiliteSetProvider.ts
@@ -2,18 +2,18 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+/* eslint-disable @typescript-eslint/no-deprecated */
 
 import "./DisposePolyfill.js";
-import { from, Observable, shareReplay } from "rxjs";
-import { eachValueFrom } from "rxjs-for-await";
 import { ECClassHierarchyInspector, ECSqlQueryExecutor } from "@itwin/presentation-shared";
-import { createHiliteSetProvider, HiliteSet, HiliteSetProvider } from "./HiliteSetProvider.js";
-import { StorageSelectionChangeEventArgs } from "./SelectionChangeEvent.js";
-import { IMODEL_CLOSE_SELECTION_CLEAR_SOURCE, SelectionStorage } from "./SelectionStorage.js";
+import { createHiliteSetProvider, HiliteSet } from "./HiliteSetProvider.js";
+import { createIModelHiliteSetProvider } from "./IModelHiliteSetProvider.js";
+import { SelectionStorage } from "./SelectionStorage.js";
 
 /**
  * Props for creating a `CachingHiliteSetProvider` instance.
  * @public
+ * @deprecated in 1.5. Use `IModelHiliteSetProviderProps` instead.
  */
 export interface CachingHiliteSetProviderProps {
   /** Selection storage to use for retrieving the hilite set. */
@@ -34,6 +34,7 @@ export interface CachingHiliteSetProviderProps {
  *
  * @see `createCachingHiliteSetProvider`
  * @public
+ * @deprecated in 1.5. Use `IModelHiliteSetProvider` instead.
  */
 export interface CachingHiliteSetProvider {
   /** Get the current hilite set iterator for the specified imodel */
@@ -61,62 +62,15 @@ export interface CachingHiliteSetProvider {
  * Creates a hilite set provider that caches hilite set for current selection for given iModel so any subsequent
  * hilite set requests for the same iModel don't cost until selection in given selection storage changes.
  * @public
+ * @deprecated in 1.5. Use `createIModelHiliteSetProvider` instead.
  */
+/* c8 ignore start */
 export function createCachingHiliteSetProvider(props: CachingHiliteSetProviderProps): CachingHiliteSetProvider & { [Symbol.dispose]: () => void } {
-  return new CachingHiliteSetProviderImpl(props);
+  const provider = createIModelHiliteSetProvider(props);
+  return {
+    getHiliteSet: (hiliteSetProps: { imodelKey: string }) => provider.getCurrentHiliteSet(hiliteSetProps),
+    [Symbol.dispose]: () => provider[Symbol.dispose](),
+    dispose: () => provider[Symbol.dispose](),
+  };
 }
-
-class CachingHiliteSetProviderImpl implements CachingHiliteSetProvider {
-  private _selectionStorage: SelectionStorage;
-  private _hiliteSetProviders = new Map<string, HiliteSetProvider>();
-  private _cache = new Map<string, Observable<HiliteSet>>();
-  private _removeListener: () => void;
-  private _imodelProvider: (imodelKey: string) => ECClassHierarchyInspector & ECSqlQueryExecutor;
-  private _createHiliteSetProvider: typeof createHiliteSetProvider;
-
-  constructor(props: CachingHiliteSetProviderProps) {
-    this._selectionStorage = props.selectionStorage;
-    this._imodelProvider = props.imodelProvider;
-    this._removeListener = this._selectionStorage.selectionChangeEvent.addListener((args: StorageSelectionChangeEventArgs) => {
-      this._cache.delete(args.imodelKey);
-      if (args.changeType === "clear" && args.source === IMODEL_CLOSE_SELECTION_CLEAR_SOURCE) {
-        this._hiliteSetProviders.delete(args.imodelKey);
-      }
-    });
-    this._createHiliteSetProvider = props.createHiliteSetProvider ?? /* c8 ignore next */ createHiliteSetProvider;
-  }
-
-  public getHiliteSet({ imodelKey }: { imodelKey: string }): AsyncIterableIterator<HiliteSet> {
-    const imodelAccess = this._imodelProvider(imodelKey);
-    const provider = this.getHiliteSetProvider(imodelKey, imodelAccess);
-    let hiliteSet = this._cache.get(imodelKey);
-
-    if (!hiliteSet) {
-      const selectables = this._selectionStorage.getSelection({ imodelKey });
-      hiliteSet = from(provider.getHiliteSet({ selectables })).pipe(shareReplay({ refCount: true }));
-      this._cache.set(imodelKey, hiliteSet);
-    }
-
-    return eachValueFrom(hiliteSet);
-  }
-
-  public [Symbol.dispose](): void {
-    this._removeListener();
-    this._hiliteSetProviders = new Map();
-    this._cache = new Map();
-  }
-
-  /* c8 ignore next 3 */
-  public dispose(): void {
-    this[Symbol.dispose]();
-  }
-
-  private getHiliteSetProvider(imodelKey: string, imodelAccess: ECClassHierarchyInspector & ECSqlQueryExecutor) {
-    let provider = this._hiliteSetProviders.get(imodelKey);
-    if (!provider) {
-      provider = this._createHiliteSetProvider({ imodelAccess });
-      this._hiliteSetProviders.set(imodelKey, provider);
-    }
-    return provider;
-  }
-}
+/* c8 ignore end */
