3e648228efdefd2e37172527393aadc8551fe8e5
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2026-01-26 16:55:13 +0200

    lint

--- apps/full-stack-tests/src/unified-selection/learning-snippets/SelectionScopes.test.ts
+++ apps/full-stack-tests/src/unified-selection/learning-snippets/SelectionScopes.test.ts
@@ -4,10 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { expect } from "chai";
-import { insertPhysicalElement, insertPhysicalModelWithPartition, insertSpatialCategory } from "presentation-test-utilities";
+import { collect, insertPhysicalElement, insertPhysicalModelWithPartition, insertSpatialCategory } from "presentation-test-utilities";
 // __PUBLISH_EXTRACT_START__ Presentation.UnifiedSelection.SelectionScopes.Imports
 import { computeSelection } from "@itwin/unified-selection";
-import { IModelConnection } from "@itwin/core-frontend";
 import { createECSqlQueryExecutor } from "@itwin/presentation-core-interop";
 // __PUBLISH_EXTRACT_END__
 import { buildIModel } from "../../IModelUtils.js";
@@ -25,38 +24,40 @@ describe("Unified selection", () => {
       });
 
       it("Basic selection scope", async function () {
-        const { imodel, elementKey } = await buildIModel(this, async (builder) => {
+        const { imodel, ...keys } = await buildIModel(this, async (builder) => {
           const modelKey = insertPhysicalModelWithPartition({ builder, codeValue: "test model" });
           const categoryKey = insertSpatialCategory({ builder, codeValue: "test category" });
           const elementKey = insertPhysicalElement({ builder, userLabel: "test element", modelId: modelKey.id, categoryId: categoryKey.id });
           return { modelKey, categoryKey, elementKey };
         });
 
-        const elementIds = [elementKey.id];
+        const elementIds = [keys.elementKey.id];
 
         // __PUBLISH_EXTRACT_START__ Presentation.UnifiedSelection.SelectionScopes.BasicExample
         const queryExecutor = createECSqlQueryExecutor(imodel);
         const selection = computeSelection({ queryExecutor, elementIds, scope: "element" });
         // __PUBLISH_EXTRACT_END__
 
-        const selectedKeys = [];
-        for await (const key of selection) {
-          selectedKeys.push(key);
-        }
-        expect(selectedKeys).to.have.lengthOf(1);
-        expect(selectedKeys[0].className).to.eq(elementKey.className);
-        expect(selectedKeys[0].id).to.eq(elementKey.id);
+        const selectedKeys = await collect(selection);
+        expect(selectedKeys).to.deep.eq([keys.elementKey]);
       });
 
       it("Selection scope with ancestor level", async function () {
-        const { imodel, elementKey } = await buildIModel(this, async (builder) => {
+        const { imodel, ...keys } = await buildIModel(this, async (builder) => {
           const modelKey = insertPhysicalModelWithPartition({ builder, codeValue: "test model" });
           const categoryKey = insertSpatialCategory({ builder, codeValue: "test category" });
-          const elementKey = insertPhysicalElement({ builder, userLabel: "test element", modelId: modelKey.id, categoryId: categoryKey.id });
-          return { modelKey, categoryKey, elementKey };
+          const parentElementKey = insertPhysicalElement({ builder, userLabel: "test element", modelId: modelKey.id, categoryId: categoryKey.id });
+          const elementKey = insertPhysicalElement({
+            builder,
+            userLabel: "test element",
+            modelId: modelKey.id,
+            categoryId: categoryKey.id,
+            parentId: parentElementKey.id,
+          });
+          return { modelKey, categoryKey, parentElementKey, elementKey };
         });
 
-        const elementIds = [elementKey.id];
+        const elementIds = [keys.elementKey.id];
 
         // __PUBLISH_EXTRACT_START__ Presentation.UnifiedSelection.SelectionScopes.AncestorLevelExample
         const queryExecutor = createECSqlQueryExecutor(imodel);
@@ -65,14 +66,9 @@ describe("Unified selection", () => {
         const selection = computeSelection({ queryExecutor, elementIds, scope: { id: "element", ancestorLevel: 1 } });
         // __PUBLISH_EXTRACT_END__
 
-        const selectedKeys = [];
-        for await (const key of selection) {
-          selectedKeys.push(key);
-        }
         // In this case, since the element has no parent, it should return itself
-        expect(selectedKeys).to.have.lengthOf(1);
-        expect(selectedKeys[0].className).to.eq(elementKey.className);
-        expect(selectedKeys[0].id).to.eq(elementKey.id);
+        const selectedKeys = await collect(selection);
+        expect(selectedKeys).to.deep.eq([keys.parentElementKey]);
       });
     });
   });
