d7aaf84658efce5ee5adee09962ad9c1820ee69c
MartynasStrazdas <43886789+MartynasStrazdas@users.noreply.github.com>
2026-01-27 14:00:54 +0200

    remove duplicate rule

--- packages/unified-selection-react/eslint.config.cjs
+++ packages/unified-selection-react/eslint.config.cjs
@@ -30,7 +30,6 @@ module.exports = [
       "@typescript-eslint/consistent-type-imports": "error",
       "@typescript-eslint/consistent-type-exports": "error",
       "unused-imports/no-unused-imports": "error",
-      "unused-imports/no-unused-vars": ["error", { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" }],
     },
   },
   ...eslintBaseConfig,
