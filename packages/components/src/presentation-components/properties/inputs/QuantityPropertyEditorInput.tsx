78ecc27bc3df89421cd043c39ec1d403e312703e
MartynasStrazdas <43886789+MartynasStrazdas@users.noreply.github.com>
2026-03-06 12:58:49 +0200

    try fix

--- packages/components/src/presentation-components/properties/inputs/QuantityPropertyEditorInput.tsx
+++ packages/components/src/presentation-components/properties/inputs/QuantityPropertyEditorInput.tsx
@@ -107,7 +107,9 @@ const QuantityPropertyValueInput = forwardRef<PropertyEditorAttributes, Quantity
         onFocus={() => {
           setEditing(true);
           requestAnimationFrame(() => {
-            inputRef.current && inputRef.current.setSelectionRange(0, quantityValue.highPrecisionFormattedValue === inputProps.placeholder ? 0 : 9999);
+            // separate value for coverage to cover it correctly
+            const selectionEnd = quantityValue.highPrecisionFormattedValue === inputProps.placeholder ? 0 : 9999;
+            inputRef.current?.setSelectionRange(0, selectionEnd);
           });
         }}
         onKeyDown={handleKeyDown}
--- packages/components/src/test/properties/inputs/QuantityPropertyEditorInput.test.tsx
+++ packages/components/src/test/properties/inputs/QuantityPropertyEditorInput.test.tsx
@@ -11,7 +11,6 @@ import { BeUiEvent } from "@itwin/core-bentley";
 import { IModelApp } from "@itwin/core-frontend";
 import { Format, FormatType, ParseError } from "@itwin/core-quantity";
 import { KoqPropertyValueFormatter } from "@itwin/presentation-common";
-import { stubRaf } from "../../_helpers/Common.js";
 import { createTestPropertyRecord } from "../../_helpers/UiComponents.js";
 import { SchemaMetadataContextProvider } from "../../../presentation-components/common/SchemaMetadataContext.js";
 import { QuantityEditorName } from "../../../presentation-components/properties/editors/QuantityPropertyEditor.js";
@@ -50,7 +49,7 @@ describe("<QuantityPropertyEditorInput />", () => {
   >;
   let getParserSpecStub: sinon.SinonStub<Parameters<KoqPropertyValueFormatter["getParserSpec"]>, ReturnType<KoqPropertyValueFormatter["getParserSpec"]>>;
 
-  stubRaf();
+  // stubRaf();
 
   before(() => {
     getFormatterSpecStub = sinon.stub(KoqPropertyValueFormatter.prototype, "getFormatterSpec");
