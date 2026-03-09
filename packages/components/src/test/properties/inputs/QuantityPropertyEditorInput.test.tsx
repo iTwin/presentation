a40d046c1fa5a444876319bbadd33e9bf0eef3bb
MartynasStrazdas <43886789+MartynasStrazdas@users.noreply.github.com>
2026-03-09 15:33:02 +0200

    fix

--- packages/components/src/test/properties/inputs/QuantityPropertyEditorInput.test.tsx
+++ packages/components/src/test/properties/inputs/QuantityPropertyEditorInput.test.tsx
@@ -151,9 +151,12 @@ describe("<QuantityPropertyEditorInput />", () => {
     const input = await waitFor(() => getByRole("textbox") as HTMLInputElement);
     await waitFor(() => expect(input.disabled).to.be.false);
 
-    // Verify that selection logic is applied
-    await waitFor(() => expect(input.selectionEnd).to.eq(0));
-    await user.type(input, "123.4 ", { skipClick: true, initialSelectionEnd: 0 });
+    // Verify that selection logic from animation frame is applied
+    await waitFor(() => {
+      expect(document.activeElement).to.eq(input);
+      expect(input.selectionEnd).to.eq(0);
+    });
+    await user.type(input, "123.4 ", { skipClick: true });
     await user.tab();
 
     await waitFor(() => expect(input.value).to.eq("123.4 unit"));
