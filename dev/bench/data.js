55a9557f13919b29875327a12ccb4263a8575fb0
github-action-benchmark <github@users.noreply.github.com>
2026-03-19 14:53:33 +0000

    add Hierarchies benchmark (customSmallerIsBetter) benchmark result for e599915df2643be2f3fe8086a7ad293e07b9a67f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773932006450,
+  "lastUpdate": 1773932013261,
   "repoUrl": "https://github.com/iTwin/presentation",
   "entries": {
     "Benchmark": [
@@ -44831,6 +44831,162 @@ window.BENCHMARK_DATA = {
             "value": 253,
             "unit": "ms",
             "extra": "count: 12\nmax: 253\np95: 253\nmedian: 31.5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "24278440+saskliutas@users.noreply.github.com",
+            "name": "Saulius Skliutas",
+            "username": "saskliutas"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e599915df2643be2f3fe8086a7ad293e07b9a67f",
+          "message": "Replace `prettier` with `oxfmt` (#1251)\n\n* Introduce oxfmt for formatting\n\n* Run formatter\n\n* cspell\n\n* Update-extractions\n\n* Empty line after imports\n\n* Lint\n\n* Cleanup\n\n* Scoped packages with subpath",
+          "timestamp": "2026-03-19T16:50:39+02:00",
+          "tree_id": "4bb5aebd5cd0413a102cc829b1f53947622bb842",
+          "url": "https://github.com/iTwin/presentation/commit/e599915df2643be2f3fe8086a7ad293e07b9a67f"
+        },
+        "date": 1773932011933,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "flat 50k elements list",
+            "value": 4027.84,
+            "unit": "ms"
+          },
+          {
+            "name": "flat 50k elements list (P95 of main thread blocks)",
+            "value": 61,
+            "unit": "ms",
+            "extra": "count: 10\nmax: 61\np95: 61\nmedian: 31.5"
+          },
+          {
+            "name": "filtering filters with 50000 paths",
+            "value": 7428.46,
+            "unit": "ms"
+          },
+          {
+            "name": "filtering filters with 50000 paths (P95 of main thread blocks)",
+            "value": 133,
+            "unit": "ms",
+            "extra": "count: 11\nmax: 133\np95: 133\nmedian: 27"
+          },
+          {
+            "name": "grouping by label",
+            "value": 9063.15,
+            "unit": "ms"
+          },
+          {
+            "name": "grouping by label (P95 of main thread blocks)",
+            "value": 45,
+            "unit": "ms",
+            "extra": "count: 5\nmax: 45\np95: 45\nmedian: 33"
+          },
+          {
+            "name": "grouping by class",
+            "value": 9075.93,
+            "unit": "ms"
+          },
+          {
+            "name": "grouping by class (P95 of main thread blocks)",
+            "value": 35,
+            "unit": "ms",
+            "extra": "count: 6\nmax: 35\np95: 35\nmedian: 32"
+          },
+          {
+            "name": "grouping by property",
+            "value": 9696.59,
+            "unit": "ms"
+          },
+          {
+            "name": "grouping by property (P95 of main thread blocks)",
+            "value": 48,
+            "unit": "ms",
+            "extra": "count: 9\nmax: 48\np95: 48\nmedian: 32"
+          },
+          {
+            "name": "grouping by base class (10 classes)",
+            "value": 6775.95,
+            "unit": "ms"
+          },
+          {
+            "name": "grouping by base class (10 classes) (P95 of main thread blocks)",
+            "value": 64,
+            "unit": "ms",
+            "extra": "count: 17\nmax: 64\np95: 64\nmedian: 34"
+          },
+          {
+            "name": "grouping by multiple attributes",
+            "value": 24601.33,
+            "unit": "ms"
+          },
+          {
+            "name": "grouping by multiple attributes (P95 of main thread blocks)",
+            "value": 44,
+            "unit": "ms",
+            "extra": "count: 11\nmax: 44\np95: 44\nmedian: 34"
+          },
+          {
+            "name": "hide if no children required to finalize root, w/o children",
+            "value": 32947.58,
+            "unit": "ms"
+          },
+          {
+            "name": "hide if no children required to finalize root, w/o children (P95 of main thread blocks)",
+            "value": 32,
+            "unit": "ms",
+            "extra": "count: 1\nmax: 32\np95: 32\nmedian: 32"
+          },
+          {
+            "name": "hide if no children required to finalize root, w/ children",
+            "value": 145.38,
+            "unit": "ms"
+          },
+          {
+            "name": "hide if no children required to finalize root, w/ children (P95 of main thread blocks)",
+            "value": 0,
+            "unit": "ms",
+            "extra": "count: 0\nmax: N/A\np95: N/A\nmedian: N/A"
+          },
+          {
+            "name": "models tree initial (Baytown)",
+            "value": 40.23,
+            "unit": "ms"
+          },
+          {
+            "name": "models tree initial (Baytown) (P95 of main thread blocks)",
+            "value": 0,
+            "unit": "ms",
+            "extra": "count: 0\nmax: N/A\np95: N/A\nmedian: N/A"
+          },
+          {
+            "name": "models tree full (Baytown)",
+            "value": 6238.6,
+            "unit": "ms"
+          },
+          {
+            "name": "models tree full (Baytown) (P95 of main thread blocks)",
+            "value": 86,
+            "unit": "ms",
+            "extra": "count: 2\nmax: 86\np95: 86\nmedian: 71"
+          },
+          {
+            "name": "models tree creates initial filtered view for 50k target items",
+            "value": 2223.63,
+            "unit": "ms"
+          },
+          {
+            "name": "models tree creates initial filtered view for 50k target items (P95 of main thread blocks)",
+            "value": 181,
+            "unit": "ms",
+            "extra": "count: 9\nmax: 181\np95: 181\nmedian: 32"
           }
         ]
       }
