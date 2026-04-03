17d050a79b60f97a6613430ffdb9618b65820452
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-04-03 12:32:16 +0300

    [core-interop] Migrate to `vitest` (#1271)
    * Move core-interop tests to vitest
    
    * Fix build
    
    * Format
    
    * Cleanup
    
    * Add name to test project

---
+++ packages/core-interop/vitest.config.ts
@@ -0,0 +1,27 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    name: "Core-interop tests",
+    include: ["src/**/*.test.ts"],
+    testTimeout: 60000,
+    restoreMocks: true,
+    coverage: {
+      provider: "v8",
+      include: ["src/core-interop/**/*"],
+      reportsDirectory: "./lib/test/coverage",
+      reporter: ["text-summary", "lcov", "cobertura"],
+      thresholds: {
+        statements: 100,
+        functions: 100,
+        branches: 100,
+        lines: 100,
+      },
+    },
+  },
+});
