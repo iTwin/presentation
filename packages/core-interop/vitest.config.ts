948e844ed5f40a7276952001e5023c7d58a7c31b
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-04-03 12:24:01 +0300

    Add name to test project

--- packages/core-interop/vitest.config.ts
+++ packages/core-interop/vitest.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    name: "Core-interop tests",
     include: ["src/**/*.test.ts"],
     testTimeout: 60000,
     restoreMocks: true,
