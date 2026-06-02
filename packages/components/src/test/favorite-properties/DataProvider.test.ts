cca4a66d73b6cb8c13fda4b11cc7b112d19353e4
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-06-02 09:40:30 +0300

    Fix assertion

--- packages/components/src/test/favorite-properties/DataProvider.test.ts
+++ packages/components/src/test/favorite-properties/DataProvider.test.ts
@@ -57,7 +57,7 @@ describe("FavoritePropertiesDataProvider", () => {
 
     it("[deprecated] can be created without props", () => {
       // eslint-disable-next-line @typescript-eslint/no-deprecated
-      expect(new FavoritePropertiesDataProvider()).to.be.instanceOf(FavoritePropertiesDataProvider);
+      expect(new FavoritePropertiesDataProvider()).toBeInstanceOf(FavoritePropertiesDataProvider);
     });
   });
 
