fcc4cc0194be6353f53827987c59cce4063457bb
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2026-07-08 17:00:11 +0300

    format again...

--- packages/content/src/content/model/PropertyFieldMerge.ts
+++ packages/content/src/content/model/PropertyFieldMerge.ts
@@ -3,10 +3,10 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import { normalizeFullClassName, type ValueDescriptor } from "@itwin/presentation-shared";
 import { PropertyField } from "./Field.js";
 import { toSortedUniqueClassNames } from "./Utils.js";
 
-import { normalizeFullClassName, type ValueDescriptor } from "@itwin/presentation-shared";
 import type { Field } from "./Field.js";
 
 /**
