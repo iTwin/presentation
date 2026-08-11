c1f129bed0bf7fa3983652410bcf4409d7ab97cb
JonasDov <100586436+JonasDov@users.noreply.github.com>
2026-08-11 10:48:49 +0000

    Remove config for screenshots

--- packages/hierarchies-react/vitest.config.ts
+++ packages/hierarchies-react/vitest.config.ts
@@ -35,7 +35,6 @@ export default defineConfig({
             provider: playwright(),
             headless: true,
             instances: [{ browser: "chromium", viewport: { width: 800, height: 600 } }],
-            expect: { toMatchScreenshot: { comparatorName: "pixelmatch" } },
           },
         },
       },
